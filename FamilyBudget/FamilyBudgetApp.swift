d1a5c22a4e99e3f7b67d19c0b402d9e79b53b289
M Sreekanth <m.sreekanth112@gmail.com>
2023-07-19 15:19:05 +0530

    Initial Commit

---
+++ FamilyBudget/FamilyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyBudgetApp.swift
+//  FamilyBudget
+//
+//  Created by M sreekanth  on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FamilyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
